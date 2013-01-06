16fb5c62dc10c56700e9d6ad97a405acadd1ef3f
Simon Norberg <simon@pthread.se>
2013-01-06 13:58:06 +0100

    reaction can now create more than one molecule
    and when grabbing a element, its now the element on top that gets
    grabbed and not the one on the bottom in case of more than one
    element at mouse_pos

--- molecule.py
+++ molecule.py
@@ -1,4 +1,4 @@
-import os, pygame,random
+import time, os, pygame,random
 from pygame.locals import *
 
 
@@ -24,7 +24,10 @@ class Universe:
 				return "H2"
 			elif b == 'OH':
 				return "H2O"
-
+		elif a == "CH4":
+			if b == "H2O":
+				return ["CO"] + 3*["H2"]
+		
 	def molecule_table(self, molecule):
 		layout = dict()
 		if molecule == "OH":
@@ -40,6 +43,9 @@ class Universe:
 		elif molecule == "O2":
 			layout[(1,1)] = 'O'	
 			layout[(2,1)] = 'O'	
+		elif molecule == "CO":
+			layout[(1,1)] = 'C'	
+			layout[(2,1)] = 'O'
 		elif molecule == "CO2":
 			layout[(1,1)] = 'O'	
 			layout[(2,1)] = 'C'
@@ -102,33 +108,43 @@ class Universe:
 			return molecule
 
 	def is_atom(self, symbol):
-		if len(symbol) == 1: # H, O, F etc.
+		if len(symbol) == 1 and symbol.isalpha(): # H, O, F etc.
 			return True
-		elif len(symbol) == 2 and symbol[1].islower(): #Fe, Mg, Na etc.
+		elif len(symbol) == 2 and symbol.isalpha() and symbol[1].islower(): #Fe, Mg, Na etc.
 			return True
-		elif len(symbol) == 3 and symbol[1:3].islower(): #Uut, Uup, Uus etc.
+		elif len(symbol) == 3 and symbol.isalpha() and symbol[1:3].islower(): #Uut, Uup, Uus etc.
 			return True
 		else:
 			return False
 
-	def create_elements(self, elements):
+	def create_elements(self, elements, pos=None):
 		list_of_elements = list()
+		if pos != None:
+			x, y = pos
+
+		if isinstance(elements, basestring):
+			elements = [elements] #elements is a string, wrap it in a list not to confuse for
 		for element in elements:
-			list_of_elements.append(Element(element))
+			if pos != None and len(elements) > 1:
+				pos = (x + random.randint(-50,50), y + random.randint(-50, 50))
+			list_of_elements.append(Element(element, pos))
 		return tuple(list_of_elements)
 
 universe = Universe()
 
 class Element(pygame.sprite.Sprite):
 	"""Element - The universal building block of atoms and molecules"""
-	def __init__(self, symbol):
+	def __init__(self, symbol, pos=None):
 		pygame.sprite.Sprite.__init__(self)
 		self.symbol = symbol
 		self.image = universe.create_molecule(symbol)
-		self.rect = self.image.get_rect()	
-		self.rect.move_ip(random.randint(10, 600), random.randint(10, 400))
+		self.rect = self.image.get_bounding_rect()
 		self.active = False
-	
+		if pos == None:
+			self.rect.move_ip(random.randint(10, 600), random.randint(10, 400))
+		else:
+			self.rect.move_ip(pos)
+				
 
 	def update(self):
 		if self.active:	
@@ -154,18 +170,8 @@ class Element(pygame.sprite.Sprite):
 		if reaction == None:
 			reaction = universe.reaction_table(other.symbol, self.symbol)
 		if reaction != None:
-			self.transform(reaction)
-			print "Created:", reaction
-			return True
-		return False
-
-	def transform(self, new_symbol):
-		image = universe.create_molecule(new_symbol)
-		if image != None:
-			self.image = image
-			self.symbol = new_symbol
-			self.rect = self.image.get_rect()
-			
+			print "Created:", reaction, "from ", self.symbol,"+", other.symbol
+			return universe.create_elements(reaction, pos = self.rect.midtop)
 
 def main():
 	pygame.init()
@@ -187,7 +193,7 @@ def main():
 
 	clock = pygame.time.Clock()
 	e = universe.create_elements(["H", "O", "OH", "O", "H", "CO2", "CH4"])
-	elements = pygame.sprite.RenderPlain(e)
+	elements = pygame.sprite.RenderUpdates(e)
 	active = None
 	while 1:
 		clock.tick(60)
@@ -197,7 +203,7 @@ def main():
 			elif event.type == KEYDOWN and event.key == K_ESCAPE:
 				return
 			elif event.type == MOUSEBUTTONDOWN:
-				for element in elements.sprites():
+				for element in reversed(elements.sprites()):
 					if element.clicked():
 						active = element
 						break
@@ -208,11 +214,17 @@ def main():
 		if active != None:
 			for collition in pygame.sprite.spritecollide(active, elements, False):
 				if collition != active:
-					if active.react(collition):
-						collition.kill()	
-					
+					new_elements = active.react(collition)
+					if new_elements != None:
+						collition.kill()
+						active.kill()
+						active = None
+						elements.add(new_elements)
+					break	
 		elements.update()
 		screen.blit(background, (0, 0))
+		#if active != None:
+		#	pygame.draw.rect(screen, pygame.color.Color("black"), active.rect)
 		elements.draw(screen)
 		pygame.display.flip()
 
@@ -221,6 +233,7 @@ if __name__ == '__main__':
 	try:
 		main()
 	except KeyboardInterrupt:
-		from guppy import hpy
-		h = hpy()
-		print h.heap()
+		pass
+	from guppy import hpy
+	h = hpy()
+	print h.heap()
