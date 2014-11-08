4bf2c4ca07ed8d5ccfe3cbeaa2823cec388bc50c
Simon Norberg <simon@pthread.se>
2014-11-08 19:42:48 +0100

    Gui upgraded from pygtk2 to gtk3

--- cmleditor/EditorGTK.py
+++ cmleditor/EditorGTK.py
@@ -16,11 +16,13 @@
 #!/usr/bin/env python
 import sys
 import os
-import pygtk
-import glob 
-import gtk
-import gtk.glade
-import gobject
+import gi
+import glob
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository.GdkPixbuf import Pixbuf
+#import Gtk.glade
+from gi.repository import GObject
 import libcml.Cml as Cml
 import cml2img
 from subprocess import call
@@ -28,9 +30,9 @@ import time
 class EditorGTK:
 
     def __init__(self):
-        sys.excepthook = self.excepthook    
+        sys.excepthook = self.excepthook
         self.gladefile = "cmleditor/gui.gtkbuilder"
-        self.builder = gtk.Builder()
+        self.builder = Gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         self.widget = self.builder.get_object
         self.window = self.widget("winMain").show()
@@ -42,47 +44,46 @@ class EditorGTK:
     def init_twStates(self):
         twStates = self.widget("twStates")
         #set what data type twStates shulld contain
-        self.modelStates = gtk.ListStore(gobject.TYPE_STRING,\
-        gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        self.modelStates = Gtk.ListStore(str, str, str, str)
         twStates.set_model(self.modelStates)
-        self.modelStateNames = gtk.ListStore(gobject.TYPE_STRING)
+        self.modelStateNames = Gtk.ListStore(str)
         self.modelStateNames.append(["Solid"])
         self.modelStateNames.append(["Aqueous"])
         self.modelStateNames.append(["Gas"])
         self.modelStateNames.append(["Liquid"])
-        cell = gtk.CellRendererText()
+        cell = Gtk.CellRendererText()
         cmbStates = self.widget("cmbStates")
         cmbStates.set_model(self.modelStateNames)
         cmbStates.pack_start(cell, True)
         cmbStates.add_attribute(cell, "text", 0)
         #create columns
-        edit0 = gtk.CellRendererText()
+        edit0 = Gtk.CellRendererText()
         #edit0.set_property('editable', True)
-        edit0.connect('edited', self.edited_string, (self.modelStates, 0))
-        edit1 = gtk.CellRendererText()
+        #edit0.connect('edited', self.edited_string, (self.modelStates, 0))
+        edit1 = Gtk.CellRendererText()
         edit1.set_property('editable', True)
         edit1.connect('edited', self.edited_float, (self.modelStates, 1))
-        edit2 = gtk.CellRendererText()
+        edit2 = Gtk.CellRendererText()
         edit2.set_property('editable', True)
         edit2.connect('edited', self.edited_float, (self.modelStates, 2))
-        edit3 = gtk.CellRendererText()
+        edit3 = Gtk.CellRendererText()
         edit3.set_property('editable', True)
         edit3.connect('edited', self.edited_ions, (self.modelStates, 3))
 
-        col1 = gtk.TreeViewColumn("State", edit0, text=0)
-        col2 = gtk.TreeViewColumn("Enthalpy", edit1, text=1)
-        col3 = gtk.TreeViewColumn("Entropy", edit2, text=2)
-        col4 = gtk.TreeViewColumn("Ions", edit3, text=3)
+        col1 = Gtk.TreeViewColumn("State", edit0, text=0)
+        col2 = Gtk.TreeViewColumn("Enthalpy", edit1, text=1)
+        col3 = Gtk.TreeViewColumn("Entropy", edit2, text=2)
+        col4 = Gtk.TreeViewColumn("Ions", edit3, text=3)
         twStates.append_column(col1)
         twStates.append_column(col2)
         twStates.append_column(col3)
         twStates.append_column(col4)
 
     def on_winMain_destroy(self, widget):
-        gtk.main_quit()
+        Gtk.main_quit()
 
     def on_btnExit_clicked(self, widget):
-        gtk.main_quit()
+        Gtk.main_quit()
 
     def on_btnSave_clicked(self, widget):
         self.molecule.states = dict()
@@ -90,7 +91,7 @@ class EditorGTK:
             name = col[0]
             enthalpy = col[1] if col[1] != "" else None
             entropy = col[2] if col[2] != "" else None
-            ions = col[3].split(',') if col[3] != "" else None    
+            ions = col[3].split(',') if col[3] != "" else None
             state = Cml.State(name, enthalpy, entropy, ions)
             self.molecule.states[name] = state
         self.molecule.property["Name"] = self.txtMoleculeName.get_text()
@@ -126,7 +127,7 @@ class EditorGTK:
             self.folder = new_folder
             self.update_folder_list()
 
-        self.openFile(widget.get_filename())    
+        self.openFile(widget.get_filename())
 
     def openFile(self, filename):
         self.filename = filename
@@ -134,13 +135,13 @@ class EditorGTK:
         molecule = Cml.Molecule()
         self.molecule = molecule
         molecule.parse(filename)
-        formula = filename.split("/")[-1].split(".cml")[0] 
+        formula = filename.split("/")[-1].split(".cml")[0]
         state_formula = formula+"(g)"
         cml2img.convert_cml2png(state_formula, "preview.png")
-        pixBuffPreview = gtk.gdk.pixbuf_new_from_file("preview.png")
+        pixBuffPreview = Pixbuf.new_from_file("preview.png")
         imgPreview = self.widget("imgPreview")
         imgPreview.set_from_pixbuf(pixBuffPreview)
-        
+
         tableWriteable = self.widget("tableWriteable")
         self.emptyContainer(tableWriteable)
         self.txtMoleculeName = self.createAndAttachTextBox("Name:",
@@ -151,21 +152,21 @@ class EditorGTK:
             self.readAtomSettings()
         else:
             pass
-        
+
         self.modelStates.clear()
         for state in molecule.states.values():
             stateList = [state.name, state.enthalpy, state.entropy, state.ions_str]
-            stateList = [x if x is not None else "" for x in stateList]
+            stateList = [str(x) if x is not None else "" for x in stateList]
             self.modelStates.append(stateList)
-    
+
     def readAtomSettings(self):
         tableWriteable = self.widget("tableWriteable")
         molecule = self.molecule
         self.txtAtomWeight = self.createAndAttachTextBox("Weight:",
-                                   tableWriteable,1)        
+                                   tableWriteable,1)
         self.txtAtomWeight.set_text(str(molecule.property.get("Weight","")))
         self.txtAtomRadius = self.createAndAttachTextBox("Radius:",
-                                   tableWriteable,2)        
+                                   tableWriteable,2)
         self.txtAtomRadius.set_text(str(molecule.property.get("Radius","")))
 
     def emptyContainer(self, container):
@@ -173,13 +174,13 @@ class EditorGTK:
             child.destroy()
 
     def createAndAttachLabel(self, text, table, col, row):
-        label = gtk.Label(text)
+        label = Gtk.Label(label=text)
         table.attach(label, col, col+1, row, row+1)
         label.set_visible(True)
-        
+
     def createAndAttachTextBox(self,text,table, row):
         self.createAndAttachLabel(text, table, 0, row)
-        entry = gtk.Entry()
+        entry = Gtk.Entry()
         table.attach(entry, 1, 2, row, row+1)
         entry.set_visible(True)
         return entry
@@ -202,18 +203,21 @@ class EditorGTK:
         model.set(iter, col_num,new_text)
 
     def on_btnAddState_clicked(self, widget):
-        new_state = self.widget("cmbStates").get_active_text()
+        cmbStates = self.widget("cmbStates")
+        tree_iter = cmbStates.get_active_iter()
+        model = cmbStates.get_model()
+        new_state = model[tree_iter][0]
         if not self.state_already_added(new_state):
-            self.modelStates.append([new_state, "", "", ""])    
+            self.modelStates.append([new_state, "","", ""])
 
     def state_already_added(self, statename):
         for state in self.modelStates:
             if state[0] == statename:
                 return True
         return False
-    
+
     def on_twStates_key_press_event(self,widget, userdata):
-        if gtk.gdk.keyval_name(userdata.keyval) == "Delete":
+        if Gdk.keyval_name(userdata.keyval) == "Delete":
             model, iter = self.widget("twStates").get_selection().get_selected()
             if iter:
                 model.remove(iter)
@@ -232,28 +236,28 @@ class EditorGTK:
         ret = call(["obabel", "-:%s" % smiles, "-h", "--gen2d", "-ocml", "-O", path])
         MsgBox("Creating molecule...")
         self.update_folder_list()
-        self.widget("fcbOpen").set_filename(path)    
+        self.widget("fcbOpen").set_filename(path)
         self.on_fcbOpen_file_set(self.widget("fcbOpen"))
 
     def excepthook(self, type, value, traceback):
         MsgBox("Error:"+ str(type) +"\n"+ str(value))
         sys.__excepthook__(type, value, traceback)
 
-        
+
 
 
 def MsgBox(message):
-        dialog = gtk.MessageDialog(None,
-                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                gtk.MESSAGE_INFO, gtk.BUTTONS_OK,
+        dialog = Gtk.MessageDialog(None,
+                Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                Gtk.MessageType.INFO, Gtk.ButtonsType.OK,
                 message)
         dialog.run()
         dialog.destroy()
 
 def YesNo(message):
-        dialog = gtk.MessageDialog(None,
-                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO,
+        dialog = Gtk.MessageDialog(None,
+                Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                Gtk.MessageType.QUESTION, Gtk.ButtonsType.YES_NO,
                 message)
         response = dialog.run()
         dialog.destroy()
@@ -264,45 +268,45 @@ def YesNo(message):
 
 def responseToDialog(entry, dialog, response):
     dialog.response(response)
-    
+
 def InputBox(title, questions):
-    dialog = gtk.Dialog(title, None, 0,
-    (gtk.STOCK_OK, gtk.RESPONSE_OK,
-    "Cancel", gtk.RESPONSE_CANCEL))
+    dialog = Gtk.Dialog(title, None, 0,
+    (Gtk.STOCK_OK, Gtk.ResponseType.OK,
+    "Cancel", Gtk.ResponseType.CANCEL))
     dialog.set_resizable(False)
-    hbox = gtk.HBox(False, 8)
+    hbox = Gtk.HBox(False, 8)
     hbox.set_border_width(8)
     dialog.vbox.pack_start(hbox, False, False, 0)
 
-    stock = gtk.image_new_from_stock(
-            gtk.STOCK_DIALOG_QUESTION,
-            gtk.ICON_SIZE_DIALOG)
+    stock = Gtk.Image.new_from_stock(
+            Gtk.STOCK_DIALOG_QUESTION,
+            Gtk.IconSize.DIALOG)
     hbox.pack_start(stock, False, False, 0)
-    table = gtk.Table(2, 2)
+    table = Gtk.Table(2, 2)
     table.set_row_spacings(4)
     table.set_col_spacings(4)
     hbox.pack_start(table, True, True, 0)
     entryBoxes = list()
     pos = 0
     for question in questions:
-        label = gtk.Label(question)
+        label = Gtk.Label(label=question)
         label.set_use_underline(True)
         table.attach(label, 0, 1, pos, pos+1)
-        entry = gtk.Entry()
-        entry.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
+        entry = Gtk.Entry()
+        entry.connect("activate", responseToDialog, dialog, Gtk.ResponseType.OK)
         table.attach(entry, 1, 2, pos, pos+1)
         label.set_mnemonic_widget(entry)
         entryBoxes.append(entry)
         pos += 1
-        
+
     dialog.show_all()
     response = dialog.run()
-    if response == gtk.RESPONSE_OK:
+    if response == Gtk.ResponseType.OK:
             answers = list()
     else:
         dialog.destroy()
         return None
-    
+
     for entryBox in entryBoxes:
         answers.append(entryBox.get_text())
     dialog.destroy()
