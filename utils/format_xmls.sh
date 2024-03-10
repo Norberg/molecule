21eafc8cf2166fe0888c12c1ec072279350269dd
Simon Norberg <simon@pthread.se>
2024-03-10 12:48:46 +0100

    Only update files needing reformating with format_xmls

--- utils/format_xmls.sh
+++ utils/format_xmls.sh
@@ -1,6 +1,15 @@
+#!/bin/sh
 FILES=data/molecule/*.cml
 
 for f in $FILES
 do
-	xmllint --format "$f" --output "$f"
+  TEMP=$(mktemp) # Create a temporary file for holding formatted content
+  xmllint --format "$f" --output "$TEMP" # Format the original file content to the temp file
+  # Compare the temporary file with the original to check for changes
+  if ! cmp -s "$TEMP" "$f"; then
+    mv "$TEMP" "$f" # Replace original file with the temporary file if there are differences
+    echo "Updated: $f" # Print information only if the file was reformatted
+  else
+    rm "$TEMP" # Delete the temporary file if no changes were made
+  fi
 done
