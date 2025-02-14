2bb529e956e1e6b784c8fbba7689f0ef1e8be8b3
Simon Norberg <simon@pthread.se>
2025-02-14 16:09:00 +0100

    Temp commit

--- molecule/server/routes.py
+++ molecule/server/routes.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from libcml import Cml
+from fastapi.responses import FileResponse
 import glob
+import subprocess
+
+from libcml import Cml
+import cml2img
+#import utils.rdkit_render as rdkit_render
 
 
 router = APIRouter()
@@ -12,6 +17,21 @@ async def get_game_state():
 @router.get("/molecule")
 async def getMolecules():
     return [getMolecule(filename) for filename in glob.glob("data/molecule/*")]
+
+
+@router.get("/molecule/{formula}/image")
+async def getMoleculeImage(formula):
+    state_formula = formula+"(aq)"
+    #cml2img.convert_cml2ipng(state_formula, "preview.png")
+    ## run cmd like python cml2img.py -f "H2O(aq)" -o preview.png
+    subprocess.run(["python", "cml2img.py", "-f", state_formula, "-o", "preview.png"])
+    return FileResponse("preview.png")
+
+@router.get("/molecule/{formula}/skeletal")
+async def getMoleculeSkeletal(formula):
+    rdkit_render.render_individual_molecule(formula, None, "preview-skeletal.png")
+    return FileResponse("preview-skeletal.png")
+
 
 def getMolecule(filename):
     molecule = Cml.Molecule()
