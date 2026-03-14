a3a06f57065bb91a14905eedfcdfa2ea8035ae85
Simon Norberg <simon@pthread.se>
2026-03-14 17:51:07 +0100

    runTest.sh run once if not passing -i or --loop

--- runTest.sh
+++ runTest.sh
@@ -1,6 +1,18 @@
-while true; do
+#!/usr/bin/env sh
+
+set -eu
+
+run_once() {
 	python3 -m mypy
 	python3 -m unittest discover
 	date
-	inotifywait -q -e modify -r  . --exclude '(.pyc|.swp|.png)'
-done
+}
+
+if [ "${1:-}" = "-l" ] || [ "${1:-}" = "--loop" ]; then
+	while true; do
+		run_once
+		inotifywait -q -e modify -r . --exclude '(.pyc|.swp|.png)'
+	done
+else
+	run_once
+fi
